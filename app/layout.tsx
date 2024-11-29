992b5731b88f9e81878e79b9586698e3675ab307
Ashwin-3cS <flashashwin2004@gmail.com>
2024-11-29 07:42:47 +0530

    edited layout with rainbowKit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Navbar } from "@/components/navbar";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  sepolia,
+} from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,11 +26,31 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
+    throw new Error("NEXT_PUBLIC_PROJECT_ID is not defined");
+  }
+  const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+
+  const config = getDefaultConfig({
+    appName: "Hospital Dapp",
+    projectId: projectId,
+    chains: [sepolia, mainnet, polygon, optimism, arbitrum, base],
+    ssr: true,
+  });
+
+  const queryClient = new QueryClient();
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider>
+              <Navbar />
+              {children}
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
       </body>
     </html>
   );
